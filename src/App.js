5ff8678339ea8d4c7cd0247c8d55a555c40214dd
nofarbendavid <nofarbendavid@gmail.com>
2020-02-06 01:50:48 +0200

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NUMBER_FACT_MAP} from "./utils";
 import {useState} from "./index";
 import './App.css';
@@ -6,6 +6,11 @@ import './App.css';
 const App = () => {
   const [count, setCount] = useState(0);
   const [funFact, setFunFact] = useState();
+
+
+  useEffect(() => {
+    document.title = `${count} clicks`;
+  }, [count]);
 
   const handleClick = () => {
     setCount(count + 1);
@@ -27,7 +32,13 @@ const App = () => {
 
         </div>
       </header>
-        {funFact && <h4 className="fact-container">Did you know that {count} {funFact}</h4>}
+        {
+          funFact &&
+        <div>
+          <h4 className="fact-container">Did you know that {count} {funFact}</h4>
+          <button onClick={() => {setFunFact(null)}}>Clear</button>
+        </div>
+        }
     </div>
   );
 };
