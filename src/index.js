3c587362b4486e7497bea34183d157d0e1796894
nofarbendavid <nofarbendavid@gmail.com>
2020-02-09 20:07:33 +0200

    add prettier. add cleanup to useEffect. rename currentPointer to 'index' in custom useState and useEffect.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {isEmpty} from 'lodash';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import { isUndefined } from "lodash";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
 const STATE = [];
 let currentStatePointer = -1;
@@ -10,40 +10,42 @@ let currentStatePointer = -1;
 const DEPENDENCIES = [];
 let currentDependenciesPointer = -1;
 
-export const useState = (initialState) => {
-  const currentPointer = ++currentStatePointer;
+export const useState = initialState => {
+  const index = ++currentStatePointer;
 
-  if(STATE[currentPointer]){
-    return STATE[currentPointer]
+  if (STATE[index]) {
+    return STATE[index];
   }
 
-  const setState = (newState) => {
-    STATE[currentPointer][0] = newState;
-    customRender()
+  const setState = newState => {
+    STATE[index][0] = newState;
+    customRender();
   };
 
-  const nextState =  [initialState, setState];
-  STATE[currentPointer] = nextState;
+  const nextState = [initialState, setState];
+  STATE[index] = nextState;
 
   return nextState;
-}
+};
 
 export const useEffect = (callback, dependenciesArray) => {
-  const currentPointer = ++currentDependenciesPointer;
-  const prevDependencies = DEPENDENCIES[currentPointer];
-  const isSimilar = prevDependencies && prevDependencies.every((dep, index)=> dep === dependenciesArray[index]);
+  const index = ++currentDependenciesPointer;
 
-  if(isEmpty(dependenciesArray) || !isSimilar){
+  const prevDependencies = DEPENDENCIES[index];
+  const isSimilar =
+    prevDependencies &&
+    prevDependencies.every((dep, index) => dep === dependenciesArray[index]);
+
+  if (isUndefined(dependenciesArray) || !isSimilar) {
     callback();
-    DEPENDENCIES[currentPointer] = dependenciesArray;
+    DEPENDENCIES[index] = dependenciesArray;
   }
-}
+};
 
 export const customRender = () => {
   currentStatePointer = -1;
   currentDependenciesPointer = -1;
-  ReactDOM.render(<App />, document.getElementById('root'));
-}
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
 
 customRender();
-
